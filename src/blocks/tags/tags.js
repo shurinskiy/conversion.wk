e4ffc24856f54604eff90f029969cfde6147a235
_tango <shurinskiy@mail.ru>
2022-12-04 19:01:41 +0600

    add article.html

--- src/blocks/tags/tags.js
+++ src/blocks/tags/tags.js
@@ -3,50 +3,55 @@ import Swiper, { Mousewheel, FreeMode } from 'swiper';
 import { throttle } from "../../js/lib";
 
 (() => {
-	const parent = document.querySelector('.tags__inner');
-	const items = parent.querySelectorAll('.tags__item');
-	const button = parent.querySelector('button.tags__more');
-	let swiper;
 
-	button.addEventListener('click', function() {
-		items.forEach(item => item.style.display = 'block');
-		parent.style.flexWrap = 'wrap';
-		this.style.display = 'none';
-	});
-	
-	const setCount = throttle(() => {
-		let count = [...items].filter((item) => {
-			return window.getComputedStyle(item).display == 'none';
-		}).length;
+	const tags = document.querySelector('.tags');
 
-		items.forEach(item => item.removeAttribute('style'));
-		parent.removeAttribute('style');
-		button.removeAttribute('style');
-		button.querySelector('span').innerText = count;
-	}, 500);
+	if(tags) {
+		const inner = tags.querySelector('.tags__inner');
+		const items = tags.querySelectorAll('.tags__item');
+		const button = tags.querySelector('button.tags__more');
+		let swiper;
 
-	
-	enquire.register("screen and (max-width:780px)", {
-		match: () => {
-			swiper = new Swiper(".tags", {
-				modules: [Mousewheel, FreeMode],
-				freeMode: true,
-				mousewheel: true,
-				spaceBetween: 15,
-				slidesPerView: 'auto'
-			});
-			window.removeEventListener("resize", setCount);
-		},
+		button.addEventListener('click', function() {
+			items.forEach(item => item.style.display = 'block');
+			inner.style.flexWrap = 'wrap';
+			this.style.display = 'none';
+		});
 		
-		unmatch: () => {
-			if (swiper !== undefined ) swiper.destroy(true, false);
-			window.addEventListener("resize", setCount);
-		},
+		const setCount = throttle(() => {
+			let count = [...items].filter((item) => {
+				return window.getComputedStyle(item).display == 'none';
+			}).length;
+
+			items.forEach(item => item.removeAttribute('style'));
+			inner.removeAttribute('style');
+			button.removeAttribute('style');
+			button.querySelector('span').innerText = count;
+		}, 500);
+
 		
-		setup: () => {
-			setCount();
-			window.addEventListener("resize", setCount);
-		}
-	});
+		enquire.register("screen and (max-width:780px)", {
+			match: () => {
+				swiper = new Swiper(tags, {
+					modules: [Mousewheel, FreeMode],
+					freeMode: true,
+					mousewheel: true,
+					spaceBetween: 15,
+					slidesPerView: 'auto'
+				});
+				window.removeEventListener("resize", setCount);
+			},
+			
+			unmatch: () => {
+				if (swiper !== undefined ) swiper.destroy(true, false);
+				window.addEventListener("resize", setCount);
+			},
+			
+			setup: () => {
+				setCount();
+				window.addEventListener("resize", setCount);
+			}
+		});
+	}
 
 })();
