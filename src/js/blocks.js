87d4a7a649ec85b4653c72a1f95b58e170af5435
_tango <shurinskiy@mail.ru>
2022-12-05 09:32:00 +0600

    add article.html

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -3,3 +3,4 @@ import "../blocks/categories/categories.js";
 import "../blocks/rating/rating.js";
 import "../blocks/code/code.js";
 import "../blocks/tags/tags.js";
+import "../blocks/aside/aside.js";
